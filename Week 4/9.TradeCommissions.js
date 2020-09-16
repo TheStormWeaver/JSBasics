80599404ee832785e719e9780e63dc4e1e1d4f78
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-16 16:07:10 +0300

    Create 9.TradeCommissions.js

---
+++ Week 4/9.TradeCommissions.js
@@ -0,0 +1,45 @@
+function solve(input) {
+  let city = input.shift();
+  let sales = +input.shift();
+  let comision = 0;
+  switch (city) {
+    case "Sofia":
+      if (sales >= 0 && sales <= 500) {
+        comision = 0.05;
+      } else if (sales >= 500 && sales <= 1000) {
+        comision = 0.07;
+      } else if (sales >= 1000 && sales <= 10000) {
+        comision = 0.08;
+      } else if (sales > 10000) {
+        comision = 0.12;
+      }
+      break;
+    case "Varna":
+      if (sales >= 0 && sales <= 500) {
+        comision = 0.045;
+      } else if (sales >= 500 && sales <= 1000) {
+        comision = 0.075;
+      } else if (sales >= 1000 && sales <= 10000) {
+        comision = 0.1;
+      } else if (sales > 10000) {
+        comision = 0.13;
+      }
+      break;
+    case "Plovdiv":
+      if (sales >= 0 && sales <= 500) {
+        comision = 0.055;
+      } else if (sales >= 500 && sales <= 1000) {
+        comision = 0.08;
+      } else if (sales >= 1000 && sales <= 10000) {
+        comision = 0.12;
+      } else if (sales > 10000) {
+        comision = 0.145;
+      }
+      break;
+  }
+  if (comision > 0) {
+    console.log(`${(comision * sales).toFixed(2)}`);
+  } else {
+    console.log("error")
+  }
+}
