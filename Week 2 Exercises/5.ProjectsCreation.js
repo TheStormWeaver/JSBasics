24a4811c0f6811fe186ff082227ebd5c1c398078
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-16 16:46:04 +0300

    Update 5.ProjectsCreation.js

--- Week 2 Exercises/5.ProjectsCreation.js
+++ Week 2 Exercises/5.ProjectsCreation.js
@@ -3,3 +3,4 @@ function createproject(input){
     let count = Number(input.shift());
     console.log(`The architect ${name} will need ${count*3} hours to complete ${count} project/s.`)
 }
+createproject(["George","4"])
