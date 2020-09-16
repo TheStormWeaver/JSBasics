70afd97f7b8cb458f63ba0510704bef53534604b
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-16 16:01:12 +0300

    Create 2.AnimalType.js

---
+++ Week 4/2.AnimalType.js
@@ -0,0 +1,15 @@
+function solve(input) {
+  let animal = input.shift();
+  switch (animal) {
+    case "dog":
+      console.log("mammal");
+      break;
+    case "snake":
+    case "crocodile":
+    case "tortoise":
+      console.log("reptile");
+      break;
+    default:
+      console.log("unknown");
+  }
+}
