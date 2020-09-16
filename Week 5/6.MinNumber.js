85b58a6e426a1cadd5c80fdc7025eac0a9ef1041
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-16 18:39:17 +0300

    Create 6.MinNumber.js

---
+++ Week 5/6.MinNumber.js
@@ -0,0 +1,14 @@
+function solve(input){
+  let n = Number(input.shift()); 
+  let counter = 0;
+  let min = Number.MAX_SAFE_INTEGER;
+  while (counter < n) {
+  let num = Number(input.shift());
+  counter++;
+  if (num < min) {
+  min = num;
+  }
+  }
+  console.log(min);
+  }
+solve(["2","100","99"])
