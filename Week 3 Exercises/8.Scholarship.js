cef586265f2855619a94b5adc86a07c453a47116
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-16 15:50:32 +0300

    Create 8.Scholarship.js

---
+++ Week 3 Exercises/8.Scholarship.js
@@ -0,0 +1,22 @@
+function solve(input) {
+  let salary = +input.shift();
+  let grades = +input.shift();
+  let minsalary = +input.shift();
+  let socialscholarship = 0;
+  let gradesscholarship = 0;
+  if (salary < minsalary) {
+    if (grades > 4.50) {
+      socialscholarship = minsalary * 0.35
+    }
+  }
+  if (grades >= 5.5) {
+    gradesscholarship = grades * 25
+  }
+  if (socialscholarship > gradesscholarship) {
+    console.log(`You get a Social scholarship ${Math.floor(socialscholarship)} BGN`)
+  } else if (gradesscholarship > socialscholarship) {
+    console.log(`You get a scholarship for excellent results ${Math.floor(gradesscholarship)} BGN`)
+  } else {
+    console.log(`You cannot get a scholarship!`)
+  }
+}
