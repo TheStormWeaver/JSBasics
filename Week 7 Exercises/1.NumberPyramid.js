208ad777e5bdb01d562a335f6fcdbb37c81369d4
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-16 19:40:09 +0300

    Create 1.NumberPyramid.js

---
+++ Week 7 Exercises/1.NumberPyramid.js
@@ -0,0 +1,22 @@
+function solve(input) {
+  let end = +input.shift();
+  let currentnumber = 1;
+  let isbigger = false;
+  let line = "";
+  for (let row = 1; row <= end; row++) {
+    for (let number = 1; number <= row; number++) {
+      if (currentnumber > end) {
+        isbigger = true;
+        break;
+      }
+      line += currentnumber + " ";
+      currentnumber++;
+    }
+    console.log(line);
+    line = ""
+    if (isbigger) {
+      break;
+    }
+  }
+}
+solve(["7"])
