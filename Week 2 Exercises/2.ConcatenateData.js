700993f0c762440cabc98bced7369d6ee50a7c71
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-16 16:39:36 +0300

    Update 2.ConcatenateData.js

--- Week 2 Exercises/2.ConcatenateData.js
+++ Week 2 Exercises/2.ConcatenateData.js
@@ -5,3 +5,4 @@ function concatenateData(input) {
     let town = input.shift();
     console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
 }
+concatenateData(["Maria","Ivanova","23","Sofia"])
